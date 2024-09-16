f7175a7b19c4b4e9cb61b4eb79db93369c6db25c
Bruce Liu <netshow-vip@sohu.com>
2024-09-16 11:26:19 +0800

    Add files via upload

---
+++ content.js
@@ -0,0 +1,7 @@
+/* browser.runtime.sendMessage(
+  {
+    // 里面的值应该可以自定义，用于判断哪个请求之类的
+    type: 'fetch',
+    url: "http://www.zhangxiuqin.top/s/index.asp?act=save&URL=33333" // 需要请求的url
+  },
+  response => JSON.parse(response.text())); */
